82d70b4d90a5b18a3e29e2fb9f98b2a0ccfc53d0
Marc ATOM <marcmartinho@gmail.com>
2021-03-24 20:27:39 +0200

    changes to secret key

--- config.py
+++ config.py
@@ -5,18 +5,19 @@ import os
 load_dotenv()
 
 class Config:
-    
+
+
     # app general
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY')
     TEMPLATES_AUTO_RELOAD = True
     STATIC_FOLDER = 'static'
     TEMPLATES_FOLDER = 'templates'
-    
+
     # app session
     SESSION_FILE_DIR = mkdtemp()
     SESSION_PERMANENT = False
     SESSION_TYPE = "filesystem"
-    
+
     # database
     SQLALCHEMY_DATABASE_URI = "sqlite:///stocks.db"
     SQLALCHEMY_ECHO = False
