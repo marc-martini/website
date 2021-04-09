f02ce865b23a187c0fe06c2dd9bf9c58fb05d99e
Marc ATOM <marcmartinho@gmail.com>
2021-04-09 23:33:35 +0200

    ke

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class Config:
 
     # database
     password = os.getenv('mysql_key')
-    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://MarcMartinho:{password}@MarcMartinho.mysql.pythonanywhere-services.com/users".format(password)
+    SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://MarcMartinho:{}@MarcMartinho.mysql.pythonanywhere-services.com/users".format(password)
     #SQLALCHEMY_DATABASE_URI = "mysql://MarcMartinho:{DB_PW}@MarcMartinho.mysql.pythonanywhere-services.com/users.db".format(DB_PW)
     SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
